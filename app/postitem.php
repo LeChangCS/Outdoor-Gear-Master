c10110114f66654a12b4e34f293cf81cf0ac7bb4
Jason Si <jasonsi1993@gmail.com>
2016-11-03 17:38:21 -0600

    add Post pictures

--- app/postitem.php
+++ app/postitem.php
@@ -5,27 +5,23 @@ $DB_ROOT = 'group';
 $DB_PASSWORD = '12345';
 $DB_NAME = 'signup';
 
-
 $price = $_POST['price'];
-$Start_Time = date("Y-d-m", strtotime(['Start_Time']));
-$End_Time = date("Y-d-m", strtotime(['End_Time']));
+$Start_Time = date('m/d/Y', strtotime($_POST['Start_Time']));
+$End_Time = date("m/d/Y", strtotime($_POST['End_Time']));
 $CheckBoxAgreement = $_POST['CheckBoxAgreement'];
 $Description = $_POST['Description'];
 $Title = $_POST['Title'];
 $Category = $_POST['categories'];
-
-$con=mysqli_connect($DB_HOST, $DB_ROOT, $DB_PASSWORD, $DB_NAME);
-
-
-
+$image = addslashes(file_get_contents($_FILES['image']['tmp_name'])); //SQL Injection defence!
+$image_name = addslashes($_FILES['image']['name']);
+$con=mysqli_connect($DB_HOST, $DB_ROOT, $DB_PASSWORD, $DB_NAME);        
 if ($conn->connect_error) {
     die("Connection failed: ". $conn->connect_error);
 } 
 mysqli_select_db($con, $DB_NAME);
 
-
-mysqli_query($con,"INSERT INTO postitem (`price`, `Start_Time`, `End_Time`, `CheckBoxAgreement`,`Description`,`Title`,`Category`)
-VALUES ('$price','$Start_Time', '$End_Time', '$CheckBoxAgreement','$Description','$Title','$Category')")
+mysqli_query($con,"INSERT INTO postitem (`price`, `Start_Time`, `End_Time`, `CheckBoxAgreement`,`Description`,`Title`,`Category`,`image`,`image_name`)
+VALUES ('$price','$Start_Time', '$End_Time', '$CheckBoxAgreement','$Description','$Title','$Category','{$image}','{$image_name}')")
 or die(mysqli_error($con));
 
 
