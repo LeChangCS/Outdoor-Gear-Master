bd7bb0c8d66cedb7135577171c13f08bee514d49
Jason Si <jasonsi1993@gmail.com>
2016-11-06 16:24:20 -0700

    post item updated

--- app/postitem.php
+++ app/postitem.php
@@ -6,8 +6,8 @@ $DB_PASSWORD = '12345';
 $DB_NAME = 'signup';
 
 $price = $_POST['price'];
-$date1 = $_POST['date1'];
-$date2 = $_POST['date2'];
+$date1 =$_POST['date1'];
+$date2 =$_POST['date2'];
 $CheckBoxAgreement = $_POST['CheckBoxAgreement'];
 $Description = $_POST['Description'];
 $Title = $_POST['Title'];
@@ -15,15 +15,13 @@ $Category = $_POST['categories'];
 $image = addslashes(file_get_contents($_FILES['image']['tmp_name'])); //SQL Injection defence!
 $image_name = addslashes($_FILES['image']['name']);
 $con=mysqli_connect($DB_HOST, $DB_ROOT, $DB_PASSWORD, $DB_NAME);        
- 
+if ($conn->connect_error) {
+    die("Connection failed: ". $conn->connect_error);
+} 
 mysqli_select_db($con, $DB_NAME);
 
 mysqli_query($con,"INSERT INTO postitem (`itemowner`, `price`, `date1`, `date2`, `CheckBoxAgreement`,`Description`,`Title`,`Category`,`image`,`image_name`)
-<<<<<<< HEAD
 VALUES ('{$_SESSION['username']}','$price','$date1', '$date2', '$CheckBoxAgreement','$Description','$Title','$Category','{$image}','{$image_name}')")
-=======
-VALUES ('{$_SESSION['username']}','$price','$Start_Time', '$End_Time', '$CheckBoxAgreement','$Description','$Title','$Category','{$image}','{$image_name}')")
->>>>>>> origin/master
 or die(mysqli_error($con));
 
 
